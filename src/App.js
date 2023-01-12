78e66523db80303eedbaf1c26acde6a9abbda84e
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-12 06:21:07 -0800

    setup and react bootstrap installation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    
+    <div className="App bg-dark text-light p-5">
+        <h1>HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
