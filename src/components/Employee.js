8a36b73e4421cbca848b4c43ee3dff7a83e9cc0c
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-13 04:09:07 -0800

    fetching data from the backend to the frontend and displaying it on the console

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,14 +1,36 @@
 import { Button, Table } from "react-bootstrap";
-import React from "react";
-import {AiOutlinePlusCircle} from 'react-icons/ai'
+import React, { useEffect, useState } from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import axios from "axios";
 const Employee = () => {
+  const [Data, setData] = useState([]);
+  const GetEmployeeData = () => {
+    const url = "http://localhost:5000";
+    axios
+      .get(url)
+      .then(response => {
+        const result = response.data;
+        const {status, message, data} = result;
+        if(status !== "SUCCESS"){
+          alert(message, status);
+        }else{
+          setData(data);
+          console.log(result);
+        }
+      })
+      .catch(err => console.log(err))
+  };
+
+  useEffect(() => {
+    GetEmployeeData();
+  }, []);
   return (
     <div>
       <div className="row">
         <div className="mt-5 mb-4">
           <Button className="mt-5 mb-4">
             <AiOutlinePlusCircle size={20} />
-               Add New Employee
+            Add New Employee
           </Button>
         </div>
       </div>
@@ -24,7 +46,6 @@ const Employee = () => {
                 <th>Action</th>
               </tr>
             </thead>
-            
           </Table>
         </div>
       </div>
